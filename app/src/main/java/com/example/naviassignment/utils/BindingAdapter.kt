34379c9fd19303de410282b73914121be93f20e4
Sandeep <sandeep@chingari.io>
2022-09-04 21:58:28 +0530

    *Added date-time formatter

--- app/src/main/java/com/example/naviassignment/utils/BindingAdapter.kt
+++ app/src/main/java/com/example/naviassignment/utils/BindingAdapter.kt
@@ -5,15 +5,18 @@ import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.naviassignment.R
+import com.example.naviassignment.utils.BindingAdapter.requiredTimeFormatter
 import com.example.naviassignment.utils.Constants.Companion.SERVER_DATE_TIME_FORMAT
-import com.example.naviassignment.utils.Constants.Companion.UI_DATE_TIME_FORMAT
+import com.example.naviassignment.utils.Constants.Companion.UI_DATE_FORMAT
+import com.example.naviassignment.utils.Constants.Companion.UI_TIME_FORMAT
 import de.hdodenhof.circleimageview.CircleImageView
 import java.text.SimpleDateFormat
 import java.util.*
 
 object BindingAdapter {
     private val serverDateFormatter = SimpleDateFormat(SERVER_DATE_TIME_FORMAT, Locale.getDefault())
-    private val requiredDateFormatter = SimpleDateFormat(UI_DATE_TIME_FORMAT, Locale.getDefault())
+    private val requiredDateFormatter = SimpleDateFormat(UI_DATE_FORMAT, Locale.getDefault())
+    private val requiredTimeFormatter = SimpleDateFormat(UI_TIME_FORMAT, Locale.getDefault())
 
     @JvmStatic
     @BindingAdapter("commitAuthorImage")
@@ -34,12 +37,12 @@ object BindingAdapter {
                     text = if (isCreated) {
                         this.context.getString(
                             R.string.created_on,
-                            requiredDateFormatter.format(date)
+                            requiredDateFormatter.format(date), requiredTimeFormatter.format(date)
                         )
                     } else {
                         this.context.getString(
                             R.string.closed_on,
-                            requiredDateFormatter.format(date)
+                            requiredDateFormatter.format(date), requiredTimeFormatter.format(date)
                         )
                     }
                 }
--- app/src/main/java/com/example/naviassignment/utils/Constants.kt
+++ app/src/main/java/com/example/naviassignment/utils/Constants.kt
@@ -3,7 +3,8 @@ package com.example.naviassignment.utils
 class Constants {
     companion object {
         const val SERVER_DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:SSS"
-        const val UI_DATE_TIME_FORMAT = "dd MMM yyyy"
+        const val UI_DATE_FORMAT = "dd MMM yyyy"
+        const val UI_TIME_FORMAT = "HH:mm:SS"
         const val COMMIT_TYPE_CLOSED="close"
     }
 
