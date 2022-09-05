ce3b4f7b8a90bec18b4100f41dcfb2f26ee40677
Sandeep <sandeep@chingari.io>
2022-09-05 15:10:40 +0530

    Text revamp for both commit author and title

--- app/src/main/java/com/example/naviassignment/data/mapper/ApiGitRepositoryMapper.kt
+++ app/src/main/java/com/example/naviassignment/data/mapper/ApiGitRepositoryMapper.kt
@@ -7,10 +7,10 @@ class ApiGitRepositoryMapper : Mapper<ApiGitResponse, GitResponse> {
     override fun toDomain(model: ApiGitResponse): GitResponse {
         return try {
             GitResponse(
-                title = model.title,
+                title = model.title?.replaceFirstChar { it.uppercaseChar() },
                 createdDate = model.createdAt,
                 closedDate = model.closedAt,
-                creatorName = model.user?.login,
+                creatorName = model.user?.login?.replaceFirstChar { it.uppercaseChar() },
                 creatorProfileImage = model.user?.avatarUrl,
                 id = model.id
             )
